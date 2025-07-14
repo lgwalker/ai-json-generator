992bc3d85846206ee3bd59471c5b894b66a0b905
Lily Walker <lgwalker@wm.edu>
2025-07-13 17:21:38 -0700

    Rename files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ai-json-generator/', // replace with your actual repo name
   server: {
     port: 3000
   }
